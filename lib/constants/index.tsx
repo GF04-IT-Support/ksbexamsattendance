fd6919566ebd261459e3b41a17209f0a4bcbe573
EpaphrasSam <isinesam@gmail.com>
2024-08-06 09:54:51 +0000

    Added TAs tabs

--- lib/constants/index.tsx
+++ lib/constants/index.tsx
@@ -4,6 +4,7 @@ import NursesIcon from "@mui/icons-material/LocalHospital";
 import ITSupportIcon from "@mui/icons-material/Computer";
 import AdministrativeIcon from "@mui/icons-material/Business";
 import AttachMoney from "@mui/icons-material/AttachMoney";
+import { FaChalkboardTeacher } from "react-icons/fa";
 
 export const StaffTabsLinks = [
   {
@@ -35,5 +36,10 @@ export const StaffTabsLinks = [
     id: "feeCollectors",
     label: "Fee Collectors",
     icon: <AttachMoney />,
+  },
+  {
+    id: "ta",
+    label: "TA",
+    icon: <FaChalkboardTeacher />,
   },
 ];
